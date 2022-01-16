a07df49d671be615a2ec3733be0a071e388ee269
Kroese <Josh.Kroese@fike.com>
2022-01-16 12:11:49 -0600

    completed adding the cards structure to the main page

---
+++ src/components/Cards.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Checkout our past work!</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem 
+                        src="images/test image 1.jpg"
+                        text="Test text"
+                        label="Test label"
+                        path="/services"
+                        />
+                        <CardItem 
+                        src="images/test image 2.jpg"
+                        text="Test text"
+                        label="Test label"
+                        path="/services"
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem 
+                        src="images/test image 1.jpg"
+                        text="Test text"
+                        label="Test label"
+                        path="/services"
+                        />
+                        <CardItem 
+                        src="images/test image 2.jpg"
+                        text="Test text"
+                        label="Test label"
+                        path="/services"
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
